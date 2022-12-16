4c9520750f4fd525d48fc0e72ea973517cee2e72
AkhshyGanesh <akhshy.balakannan@aspiresys.com>
2022-12-16 15:10:31 +0530

    Code For Persisting Redux in LS

--- website/src/app/store.js
+++ website/src/app/store.js
@@ -1,8 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
+import throttle from 'lodash.throttle';
 import counterReducer from '../features/counter/counterSlice';
+import { loadState, saveState } from './localStorage'
+
+const preloadedState = loadState();
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  preloadedState
 });
+
+store.subscribe(throttle(() => {
+  saveState({
+    counter: store.getState().counter
+  });
+}, 500));
